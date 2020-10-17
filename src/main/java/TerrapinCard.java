e038fdaaaabc2422ff49444c7e5f8d8f91898877
mattSong2 <msong1012@gmail.com>
2020-10-17 03:11:26 +0000

    changed some numbers again

--- src/main/java/TerrapinCard.java
+++ src/main/java/TerrapinCard.java
@@ -1,33 +1,37 @@
 public class TerrapinCard {
+
   private double balance;
-    public TerrapinCard(double balanceAtStart){
-      balance = balanceAtStart;
+
+  public TerrapinCard(double balanceAtStart){
+    balance = balanceAtStart;
+  }
+
+  public String toString(){
+    return "The card has " + balance + " dollars";
+  }
+
+  public void payEconomical(){
+    if (balance > 2.5){
+      balance -= 2.5;
     }
-    public String toString(){
-      return "The card has " + balance + " dollars";
+  }
+  public void payGourmet(){
+    if (balance > 4.0){
+      balance -= 4.0;
     }
-    public void payEconomical(){
-      if(balance >= 2.50){
-        balance -= 2.50;
-      }
+  }
+  public void loadMoney(double amount){
+    double addedAmount = amount;
+    if (addedAmount > 0){
+      balance += addedAmount;
+      if (balance > 150 ){
+      balance = 150;
+       }
+    }else{
+      balance = balance;
     }
-    public void payGourmet(){
-      if(balance >= 4.00){
-        balance -= 4.00;
-      }
-    }
-    public void loadMoney(double amount){
-      if(amount <= 0){
-        amount  = 0;
-        balance += amount;
-      }
-      if(amount >= 150.0){
-        balance += 150.0;
-      } else {
-        balance += amount;
-      }
-    }
-    public double getBalance(){
-      return balance;
-    }
+  }
+  public double getBalance(){
+    return balance;
+  }
 }
